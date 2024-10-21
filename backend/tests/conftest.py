e12636be74ddcf77c58d246cc36549d8e5c5ae97
Evan-Kim2028 <ekcopersonal@gmail.com>
2024-10-21 19:35:43 -0400

    fix: remove pytests

--- backend/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from fastapi.testclient import TestClient
-from main import app
-
-
-@pytest.fixture(scope="module")
-def test_client():
-    with TestClient(app) as client:
-        yield client
